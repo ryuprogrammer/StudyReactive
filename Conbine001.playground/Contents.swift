39d7b31f7b2d787d2cfb481cee166b28afc78e40
トム・クルーズ <ryu1421magic@icloud.com>
2023-07-09 18:03:49 +0900

    短縮系1

--- Conbine001.playground/Contents.swift
+++ Conbine001.playground/Contents.swift
@@ -1,6 +1,7 @@
 import Foundation
 import Combine
 
+// -MARK: 最も簡単な例
 // 整数型の値を出力するパブリッシャー
 let publisher = Just(100)
 
@@ -22,3 +23,17 @@ let subscriber = Subscribers.Sink<Int, Never>(receiveCompletion: { completion in
 
 // publisherが出力した値をsubscriberで受け取ることができる
 publisher.subscribe(subscriber)
+
+// -MARK: 短縮系1
+Just(999).sink { completion in
+    switch completion {
+    case .failure(let error):
+        print(error.localizedDescription)
+    case .finished:
+        print("終了")
+    }
+} receiveValue: { value in
+    print("受け取った値: \(value)")
+}
+
+// -MARK: 短縮系2
